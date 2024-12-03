5fd6f6cfbe792ce48d66934391c1da4b42194d6b
Josh Khrustalev <khrustlevjosh@gmail.com>
2024-12-03 12:12:53 +0300

    Add files

---
+++ precache-manifest.b94c80a8416f4cf577ab82cd7b9c5976.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fde6886a512cffeedad71195fd1027c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e127180ae76694cc278e",
+    "url": "/static/css/2.55a96ad4.chunk.css"
+  },
+  {
+    "revision": "c6aadc72c8fac7057e18",
+    "url": "/static/css/main.4a9d37e1.chunk.css"
+  },
+  {
+    "revision": "e127180ae76694cc278e",
+    "url": "/static/js/2.5671e088.chunk.js"
+  },
+  {
+    "revision": "c8637efb562c871ce30034f0ae28cfe1",
+    "url": "/static/js/2.5671e088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6aadc72c8fac7057e18",
+    "url": "/static/js/main.b52b023c.chunk.js"
+  },
+  {
+    "revision": "89b3fa8e806f368f8366",
+    "url": "/static/js/runtime-main.29d37a94.js"
+  },
+  {
+    "revision": "f112b93d50b09e18547bdc2729693212",
+    "url": "/static/media/gb.f112b93d.svg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "3c5b317470e126f7c059058d6330b35b",
+    "url": "/static/media/logo512.3c5b3174.png"
+  },
+  {
+    "revision": "483d70fa74a3ee53a808aeaa0ddd0e56",
+    "url": "/static/media/ru.483d70fa.svg"
+  }
+]);
